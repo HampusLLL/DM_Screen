2b8d51082283bf9defe38f3bf7e94bfeb02f0630
HampusLLL <112476670+HampusLLL@users.noreply.github.com>
2023-12-13 12:29:48 +0100

    all

---
+++ Monsters/Index.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DM_SHEET.Pages.Monsters
+{
+    public class IndexModel
+    {
+
+        public void OnGet()
+        {
+            // Initialization logic if needed
+        }
+    }
+}
