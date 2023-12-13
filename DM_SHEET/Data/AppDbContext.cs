c88b583d8bf13825edcefd3a03ab7464dfe1deec
HampusLLL <112476670+HampusLLL@users.noreply.github.com>
2023-12-13 12:18:20 +0100

    Done

---
+++ DM_SHEET/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DM_SHEET.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace DM_SHEET.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ DM_SHEET/Pages/Karaktärer/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DM_SHEET.Pages.Karaktärer
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
