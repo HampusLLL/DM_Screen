4b4bf71bbfb829c10060216ffb62fba0be80850f
HampusLLL <112476670+HampusLLL@users.noreply.github.com>
2023-12-13 12:21:41 +0100

    fafa

---
+++ Models/AccessControl.cs
@@ -0,0 +1,13 @@
+﻿using DM_SHEET.Data;
+using DM_SHEET.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace DM_SHEET.Data
+{
+    public class AccessControl
+    {
+        public int LoggedInAccountID { get; set; }
+        public string LoggedInAccountName { get; set; }
+    }
+}
